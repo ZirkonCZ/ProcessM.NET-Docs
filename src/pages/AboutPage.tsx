fef0eb0e7be3b21397fdb54170f5a14f7ccad7cc
Zdeněk Vychodil <vychodil.zdenek@outlook.com>
2025-06-11 12:44:56 +0200

    text edit on pages about and setup

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -4,23 +4,26 @@ const AboutPage: FC = () => {
     return (
         <div className="article">
             <h1 className="h1">What is ProcessM.NET?</h1>
+            <h2 className="h2">Overview</h2>
             <p className="p">
                 <b>ProcessM.NET</b> is a <b>C# library</b> designed to support basic process mining algorithms within the <b>.NET framework</b>. It is an open-source project released under the <b>GPL-3.0 license.</b>
             </p>
             <p className="p">
-                The library is intended for use primarily in academia and is being developed with future applications in industry in mind. The development is carried out by the Faculty of Informatics at Masaryk University.
+                The library is intended for use primarily in academia and is being developed with future applications in industry in mind. The development is carried out by the <a href="https://lasaris.fi.muni.cz" className="hyperlink">Lab of Software Architectures and Information Systems</a>, or as an abbreviation: Lasaris lab, Faculty of Informatics, Masaryk University.
             </p>
             <p className="p">
                 ProcessM.NET provides functionalities for core process mining concepts, including <b>process discovery and conformance checking</b>. It implements both imperative and declarative process mining approaches. Specific algorithms supported include the <b>Alpha algorithm</b>, Heuristic miner for imperative discovery, and <b>Optimal alignments</b> for imperative conformance checking. For declarative process mining, it supports discovery and conformance checking based on the <b>DECLARE Language</b> and its templates.
             </p>
-            <p className="p mb-4">
-                Several projects utilize or are built upon the ProcessM.NET library:
-            </p>
+            <h2 className="h2">Functionalities Supported</h2>
             <ul className="ul">
-                <li>
-                    A <b>free, web-based process mining application</b> was developed leveraging the library to make its algorithms accessible and easy to use, integrating key process mining algorithms and prioritizing accessibility and ease of use.</li>
-                <li>
-                    A new <b>C# event log generation library</b> called <b>PELGen</b> was created to produce authentic datasets, which was also published by the <a href="https://lasaris.fi.muni.cz" className="hyperlink">Faculty of Informatics, Masaryk University, Lab of Software Architectures and Information Systems.</a>
+                <li className="li">
+                    <b>Imperative Process Mining:</b> This includes algorithms for process discovery, such as the Alpha algorithm and the Heuristic miner, and conformance checking, like Optimal Alignments. Imperative models explicitly define observed behaviors.
+                </li>
+                <li className="li">
+                    <b>Declarative Process Mining:</b> This approach focuses on generating constraint-based models. The library supports declarative discovery to identify template constraints present in event logs and declarative conformance checking to evaluate how well a trace aligns with a DECLARE model by analyzing constraint activations.
+                </li>
+                <li className="li">
+                    <b>Event Log Import:</b> The library includes functionality for parsing event logs, specifically from CSV files. This was a custom C# library developed to replace a third-party package.
                 </li>
             </ul>
             <p className="p">
--- src/pages/SetupPage.tsx
+++ src/pages/SetupPage.tsx
@@ -3,43 +3,27 @@ import { FC } from "react";
 const SetupPage: FC = () => {
     return (
         <div className="article">
-            <h1 className="h1">Setup ProcessM.NET</h1>
-            <p className="par">
-                To set up and run projects utilizing the ProcessM.NET library, particularly the associated web application, you will need certain prerequisites and follow specific steps.
-            </p>
-            <p className="par mb-4">
-                Prerequisites:
-            </p>
+            <h1 className="h1">Run ProcessM.NET locally</h1>
+            
+            <h2 className="h2">Prerequisites</h2>
             <ul className="ul">
-                <li>
-                    <b>Repository</b> cloned on your machine: You need to obtain the source code for ProcessM.NET and its related projects. The source code modifications made during development are available in a public GitHub repository.
+                <li className="li">
+                    <a href="https://github.com/lasaris/ProcessM.NET" className="hyperlink">ProcessM.NET repository</a> cloned on your machine
                 </li>
-                <li>
-                    <b>Docker</b> installed: This is required if you plan to run the included web application locally using containerization.
-                </li>
-                <li>
-                    <b>.NET framework</b> installed: As ProcessM.NET is a C# library built on the .NET framework, you will need a compatible .NET environment if you intend to use the library directly in your own .NET projects for development or building the included projects from source without Docker.
+                <li className="li">
+                    <a href="https://www.docker.com/get-started/" className="hyperlink">Docker</a> installed
                 </li>
             </ul>
-                Running the Web Application Locally (using Docker):
-                The ProcessM.NET repository includes a Docker setup (compose.yaml file is mentioned) designed to streamline running the web application.
-                1.
-                Ensure you have the repository cloned to your machine.
-                2.
-                Navigate to the root directory of the cloned repository.
-                3.
-                Run the command docker compose up -d. This command builds the Docker images for the web application's frontend and API and starts them as containers in 'detached' mode (in the background).
-                4.
-                Once the containers are running, the web application should be accessible via a web browser at http://localhost:8081.
-                Using ProcessM.NET in Your Own .NET Project:
-                If you wish to use the ProcessM.NET library programmatically in your own C#/.NET application, you would typically:
-                1.
-                Obtain the source code by cloning the repository.
-                2.
-                Reference the necessary ProcessM.NET library projects (Declarative, Imperative, Import, etc.) within your own .NET solution.
-                3.
-                Utilize the classes and methods provided by the library to perform process mining operations (see the Usage page draft for a typical workflow).
-            </p>
+
+            <h2 className="h2">Steps</h2>
+            <ol className="ol">
+                <li className="li">
+                    In the root directory run <code>docker compose up -d</code>. This creates the docker containers and runs them in 'detached' mode.
+                </li>
+                <li className="li">
+                    Go to http://localhost:8081 and <b>start mining!</b>
+                </li>
+            </ol>
         </div>
     );
 }
